a05ae7c65d97764b1b76e6129169e36b7550cc38
Your Name <you@example.com>
2023-10-16 12:06:52 +0000

    avance

--- morpion.py
+++ morpion.py
@@ -2,6 +2,7 @@ import pandas as pd
 import random
 
 
+pd.set_option("display.max_columns", None)
 # def play_morpion():
 
 #     board = pd.DataFrame(
@@ -51,7 +52,7 @@ import random
 class Partie:
     """ """
 
-    def __init__(self, modele):
+    def __init__(self, modele=random.choice):
         self.players = ["X", "O"]
         self.board = pd.DataFrame(
             {
@@ -64,12 +65,15 @@ class Partie:
         self.end = False
         self.actual_player = "X"
         self.modele = modele
+        self.states = []
+        self.moves = 0
 
     def switch_player(self):
         self.actual_player = self.players[0] if self.actual_player == "O" else self.players[1]
 
     def move(self, position):
         self.board[position[0]][position[1]] = self.actual_player
+        self.moves += 1
         self.end = self.check_end(self.actual_player, position)
         self.switch_player()
 
@@ -79,11 +83,33 @@ class Partie:
         is_player_diag2 = True if position in ["a3", "b2", "c1"] else False
 
         if is_player_diag1:
-            if len(set([self.board["a"]["1"], self.board["b"]["2"], self.board["c"]["3"]])) == 1:
+            if (
+                len(
+                    set(
+                        [
+                            self.board["a"]["1"],
+                            self.board["b"]["2"],
+                            self.board["c"]["3"],
+                        ]
+                    )
+                )
+                == 1
+            ):
                 return player
 
         if is_player_diag2:
-            if len(set([self.board["a"]["3"], self.board["b"]["2"], self.board["c"]["1"]])) == 1:
+            if (
+                len(
+                    set(
+                        [
+                            self.board["a"]["3"],
+                            self.board["b"]["2"],
+                            self.board["c"]["1"],
+                        ]
+                    )
+                )
+                == 1
+            ):
                 return player
 
         if len(set(self.board[position[0]])) == 1 or len(set(self.board.loc[position[1]])) == 1:
@@ -94,30 +120,68 @@ class Partie:
 
         return False
 
+    def explore(self):
+        Q_possible = []
+        possible = []
+        for y in ["a", "b", "c"]:
+            for x in ["1", "2", "3"]:
+                if self.board.loc[x, y] == "-":
+                    Q_possible.append({"action": y + x, "gain": 0})
+                    possible.append(y + x)
+        return possible, random.choice(possible)
+
     def launch(self):
         while True:
-            possible = []
-            for y in ["a", "b", "c"]:
-                for x in ["1", "2", "3"]:
-                    if self.board.loc[x, y] == "-":
-                        possible.append(y + x)
-            position = self.modele(possible)
+            if random.random() <= 1:
+                possible, position = self.explore()
+            else:
+                possible, position = self.modele(self.player, self.board)
+            state_action = {
+                "player": self.actual_player,
+                "state": self.board.copy().to_string(),
+                "possibles": possible,
+                "action": position,
+                "move": self.moves,
+            }
+            self.states.append(state_action)
             self.move(position)
-            self.shob()
+            # self.shob()
             if isinstance(self.end, str):
-                print(self.end)
-                break
+                return {"states": self.states, "result": self.end}
 
     def shob(self):
         print(self.board)
 
 
 def train_bot():
-    Q = {}
-    for i in range(100):
+    Q = pd.DataFrame(columns=["board", "possible"])
+    ref = {"X": 1, "O": -1, "Draw": 0}
+
+    for i in range(1):
         partie = Partie(random.choice)
-        partie.launch()
+        results = partie.launch()
+        print(results["result"])
+        X_results = [d for d in results["states"] if d.get("player") == "X"]
+        for state in X_results:
+            Q = pd.concat(
+                [
+                    Q,
+                    pd.DataFrame(
+                        {
+                            "board": state["state"],
+                            "possible": [{f"{item}": 0 for item in state["possibles"]}],
+                        }
+                    ),
+                ]
+            ).reset_index(drop=True)
+
+        for st in range(len(Q) - 1, -1, -1):
+            r = ref[results["result"]] if st == len(Q) - 1 else Q.iloc[st + 1, 1][X_results[st + 1]["action"]]
+            Q.iloc[st, 1][X_results[st]["action"]] = 0.9 * r
+
+    return Q
 
 
 if __name__ == "__main__":
-    pass
+    Q = train_bot()
+    print(Q)
